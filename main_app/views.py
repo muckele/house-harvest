e696d1271fe74f3d7d45d0febafc26e6e4468634
Mathew <muckele7@gmail.com>
2024-05-23 22:59:50 -0400

    defines url and view function for about page

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from  django.http import HttpResponse
 # Create your views here.
 def home(request): 
   return HttpResponse('<h1>Hello World</h1>')
+
+def about(request):
+  return render(request, 'about.html')
